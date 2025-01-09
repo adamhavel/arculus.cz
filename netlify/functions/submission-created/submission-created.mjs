acdd09f06a79be06860cf88d9d07a52f5c1d02e9
Adam Havel <adam.havel@protonmail.com>
2025-01-09 15:01:11 +0100

    feat: update function

--- netlify/functions/submission-created/submission-created.mjs
+++ netlify/functions/submission-created/submission-created.mjs
@@ -1,26 +1,25 @@
-import { getStore } from "@netlify/blobs";
+import { getStore } from '@netlify/blobs';
 
 export default async (request, context) => {
 	try {
 		const data = await request.json();
 		const { payload } = data;
 		const { BUILD_HOOK_URL } = process.env;
-
-		console.log("Form data:", payload);
-
 		const filename = `${payload.data.id}.csv`;
 		const store = getStore("events");
 		let content = await store.get(filename);
+		
+		console.log('Form data:', payload);
 
 		content = content ? content + `\n${payload.data.email}` : `E-mail\n${payload.data.email}`;
 
 		await store.set(filename, content, {
 			metadata: {
-				"Content-Type": "text/csv",
+				'Content-Type': 'text/csv',
 			},
 		});
 
-		await fetch(BUILD_HOOK_URL, { method: "POST" });
+		await fetch(BUILD_HOOK_URL, { method: 'POST' });
 
 		return new Response(
 			JSON.stringify({
@@ -30,13 +29,13 @@ export default async (request, context) => {
 			}),
 			{
 				status: 200,
-				headers: { "Content-Type": "application/json" },
+				headers: { 'Content-Type': 'application/json' },
 			}
 		);
 	} catch (error) {
 		return new Response(error.message, {
 			status: 500,
-			headers: { "Content-Type": "application/json" },
+			headers: { 'Content-Type': 'application/json' },
 		});
 	}
 };
