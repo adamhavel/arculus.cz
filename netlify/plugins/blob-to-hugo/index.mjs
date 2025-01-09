821233e7639e72609885171473e8068fc820582f
Adam Havel <adam.havel@protonmail.com>
2025-01-09 13:10:39 +0100

    feat: add attendance logic

---
+++ netlify/plugins/blob-to-hugo/index.mjs
@@ -0,0 +1,42 @@
+import { getStore } from "@netlify/blobs";
+import { promises as fs } from "fs";
+import path from "path";
+
+export default {
+	onPreBuild: async ({ utils, constants }) => {
+		try {
+			const { SITE_ID, NETLIFY_API_TOKEN } = constants;
+			const store = getStore({
+				name: "site:events",
+				siteID: SITE_ID,
+				token: NETLIFY_API_TOKEN,
+			});
+
+			const { directories: events } = await store.list({ directories: true });
+
+			const eventEntries = await Promise.all(
+				events.map(async (directory) => {
+					const eventId = directory.replace(/\/$/, '');
+					const { blobs: attendees } = await store.list({ prefix: directory });
+					
+					return [eventId, attendees.length];
+				})
+			);
+
+			const attendance = Object.fromEntries(eventEntries);
+
+			await fs.writeFile(
+				"data/attendance.json",
+				JSON.stringify(attendance, null, 2)
+			);
+
+			console.log(
+				`✅ Created attendance.json with ${events.length} entries`
+			);
+		} catch (error) {
+			utils.build.failBuild("Failed to process form submissions", {
+				error,
+			});
+		}
+	},
+};
